4ed8c9a17e47bb08d56de86d4abdb425d90f8448
Kubatov Susa <melastdoctorwho@gmail.com>
2023-02-09 01:04:36 +0600

    init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  template: `
+    <div class="app">
+      <h1 (click)="handleClick($event)">{{ message }}</h1>
+      <h1 [innerText]="message"></h1>
+      <input [value]="message" (input)="handleInput($event)" />
+      <p>{{message}}</p>
+    </div>
+  `,
+  styles: [
+    `
+      .app {
+        text-align: center;
+        color: orange;
+        background-color: blue;
+        margin: 0 auto;
+      }
+    `,
+  ],
 })
-export class AppComponent {
-  title = 'basics';
+export class AppComponent implements OnInit {
+  message!: string;
+
+  ngOnInit(): void {
+    this.message = 'Hello Angular!';
+  }
+  handleClick(event: Event) {
+    console.log(event);
+  }
+  handleInput(event: Event) {
+    const { value } = event.target as HTMLInputElement;
+    this.message = value;
+  }
 }
